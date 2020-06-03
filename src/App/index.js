8abd5ecfcf5dcf981fef53c7863d82cf7d1644bf
Laetitia B <bourgois.laetitia@gmail.com>
2020-06-03 16:14:21 +0200

    feat: 🎸 Add button and change its coloe (theme)

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Button, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import { theme } from "./theme";
 
 export default function App() {
   return (
-    <header>
-      <h1>Pizza a Nico</h1>
-    </header>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <header>
+        <h1>Pizza a Nico</h1>
+        <Button variant="contained" color="primary">
+          Coucou
+        </Button>
+      </header>
+    </ThemeProvider>
   );
 }
---
+++ src/App/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#f4511e",
+    },
+    secondary: {
+      main: "#ffc400",
+    },
+  },
+});
